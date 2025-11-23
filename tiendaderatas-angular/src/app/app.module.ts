b699606bf2bc06c3e9639525e7b883215c2f1cfb
jagoldemberg-dotcom <ja.goldemberg@duocuc.cl>
2025-11-23 01:41:54 -0300

    feat: Activamos formularios reactivos

--- tiendaderatas-angular/src/app/app.module.ts
+++ tiendaderatas-angular/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
@@ -38,7 +38,8 @@ import { ProductService } from './services/product.service';
   ],
   imports: [
     BrowserModule,
-    FormsModule,
+    FormsModule,          // lo dejamos por compatibilidad con otras partes
+    ReactiveFormsModule,  // <--- CLAVE para formularios reactivos
     AppRoutingModule
   ],
   providers: [ProductService],
